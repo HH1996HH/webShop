4da9a4ed6c2d4ea693b424bc0eab41ae264cb606
蓝鸥科技 <lanou@hehan.local>
2018-07-28 11:07:48 +0800

    firstbuild

---
+++ src/main/java/com/lanou/model/User.java
@@ -0,0 +1,63 @@
+package com.lanou.model;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private Integer deleteid;
+
+    private Integer teacherId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Integer getDeleteid() {
+        return deleteid;
+    }
+
+    public void setDeleteid(Integer deleteid) {
+        this.deleteid = deleteid;
+    }
+
+    public Integer getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+}
